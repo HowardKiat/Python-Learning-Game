d71d6fd689ff064c6263c2e6bfc90749c237a6b2
HowardKiat <buisnesskiat123@gmail.com>
2024-09-03 14:54:01 +0800

    Bababa

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const temperatureConversion = require('./routes/temperature');
 const mysql = require('mysql2');
+const temperatureConversion = require('./routes/temperature');
+require('dotenv').config();
+
 
 const app = express();
 
@@ -10,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set 'views' directory for any views being rendered res.render()
+// Set 'views' directory for any views being rendered with res.render()
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from 'public' directory
@@ -18,36 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Set view engine as Pug
 app.set('view engine', 'pug');
-
-// Route handlers
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
-});
-
-app.get('/apple_info', (req, res) => {
-    res.render('apple_info', { title: 'Apple Info Exercise' });
-});
-
-app.get('/temperature', (req, res) => {
-    res.render('temperature', { title: 'Temperature Conversion' });
-});
-
-app.get('/responsivepage', (req, res) => {
-    res.render('responsivepage', { title: 'Responsive Page' });
-});
-
-//TODO-LIST
-const todolistRoutes = require('./routes/todolist')(db);
-app.use('/', todolistRoutes);
-
-const addRoutes = require('./routes/add')(db);
-app.use('/', addRoutes);
-
-const completetaskRoute = require('./routes/completetask')(db);
-app.use(completetaskRoute);
-
-// Handle the form submission
-app.use('/', temperatureConversion);
 
 // Data Base Connection
 const db = mysql.createConnection({
@@ -59,14 +31,43 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if (err) {
-    console.error('Database connection failed:', err);
+        console.error('Database connection failed:', err);
     } else {
-    console.log('Connected to the database');
-    }
-});
-    
+        console.log('Connected to the database');
 
-// Port Listener
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+        // Route handlers
+        app.get('/', (req, res) => {
+            res.render('index', { title: 'Home' });
+        });
+
+        app.get('/apple_info', (req, res) => {
+            res.render('apple_info', { title: 'Apple Info Exercise' });
+        });
+
+        app.get('/temperature', (req, res) => {
+            res.render('temperature', { title: 'Temperature Conversion' });
+        });
+
+        app.get('/responsivepage', (req, res) => {
+            res.render('responsivepage', { title: 'Responsive Page' });
+        });
+
+        // TODO-LIST
+        const todolistRoutes = require('./routes/todolist')(db);
+        app.use('/', todolistRoutes);
+
+        const addRoutes = require('./routes/add')(db);
+        app.use('/', addRoutes);
+
+        const completetaskRoute = require('./routes/completetask')(db);
+        app.use(completetaskRoute);
+
+        // Handle the form submission
+        app.use('/', temperatureConversion);
+
+        // Port Listener
+        app.listen(3000, function () {
+            console.log('Example app listening on port 3000!');
+        });
+    }
 });

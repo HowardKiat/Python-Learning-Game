284fe6e209ff79683b099ca93993c36318fe099d
HowardKiat <buisnesskiat123@gmail.com>
2024-10-01 13:57:23 +0800

    Update fix working ver.

--- routes/add.js
+++ routes/add.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+    // Route to add a new task
     router.post('/add', (req, res) => {
-        const { task, due_date } = req.body;
-        const query = 'INSERT INTO todolist (task, completed, due_date) VALUES (?, 0, ?)';
+        const { category, task, due_date } = req.body;
+        const query = 'INSERT INTO todolist (category, task, completed, due_date) VALUES (?, ?, 0, ?)';
 
-        db.query(query, [task, due_date], (err, results) => {
+        db.query(query, [category, task, due_date], (err, results) => {
             if (err) throw err;
             res.redirect('/todolist');
         });
     });
-    
+
     return router;
 };
-
